dbd7ffe7d9d8764d2e62e29fb21d677f2cb00608
Carloscleton <carloscleton.nat@gmail.com>
2026-08-04 09:07:30 -0300

    fix(fiscal): fix TypeScript SignedXml compilation error on Vercel

--- api/index.ts
+++ api/index.ts
@@ -622,15 +622,16 @@ app.post(['/fiscal-module/cancelar', '/api/fiscal-module/cancelar'], authenticat
 </pedRegEvento>`.trim();
 
                 // Assina o XML com o certificado
-                const sig = new SignedXml({ privateKey: privateKeyPem });
+                const sig = new SignedXml({
+                    privateKey: privateKeyPem,
+                    signatureAlgorithm: 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256',
+                    canonicalizationAlgorithm: 'http://www.w3.org/2001/10/xml-exc-c14n#'
+                });
                 sig.addReference({
                     xpath: '//*[local-name(.)="infPedReg"]',
                     transforms: ['http://www.w3.org/2000/09/xmldsig#enveloped-signature', 'http://www.w3.org/2001/10/xml-exc-c14n#'],
                     digestAlgorithm: 'http://www.w3.org/2001/04/xmlenc#sha256'
                 });
-                sig.signingKey = privateKeyPem;
-                sig.canonicalizationAlgorithm = 'http://www.w3.org/2001/10/xml-exc-c14n#';
-                sig.signatureAlgorithm = 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256';
                 
                 // Adiciona o certificado na assinatura
                 const certB64 = certPem.replace(/-----BEGIN CERTIFICATE-----|-----END CERTIFICATE-----|\r?\n/g, '');
